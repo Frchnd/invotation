eb2ba7e1e0406a3d458c8a16241ac9240af7908d
Fernando Chandra <fernandochandra2001@gmail.com>
2026-09-11 01:47:53 +0700

    Safe update

---
+++ README.txt
@@ -0,0 +1,4 @@
+INVOTATION V23 SAFE UPDATE
+
+Replace only index.html and service-worker.js in your existing GitHub repo.
+Do NOT replace cloud-config.js. Your existing Supabase connection stays intact.
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='invotation-v22-qr-pairing';
+const CACHE_NAME='invotation-v23-ui-polish';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./cloud-config.js','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
