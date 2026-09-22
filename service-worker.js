464a861329ac666a8d2f8ca8d59d152757f2e709
Fernando Chandra <fernandochandra2001@gmail.com>
2026-09-23 01:50:30 +0700

    V32 refresh app cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='invotation-v31-price-toggle-ui';
+const CACHE_NAME='invotation-v32-price-label-inline';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./cloud-config.js','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
